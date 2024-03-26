1086ed4e3909341d649549df25f4d79278c6d417
lia kaas <164993535+liakaas@users.noreply.github.com>
2024-03-26 13:42:34 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-14433188-00fb-423a-97ed-3f50be789295/)
 
 
 echo "$curl_output" >> Update.tsx
